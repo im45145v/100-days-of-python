feb1679a842acbd3e55bc8b7684c2d40a7213f6b
ashish malla <74368820+im45145v@users.noreply.github.com>
2022-05-12 17:17:27 +0530

    done for the day

--- day1.py
+++ day1.py
@@ -1,1 +1,21 @@
-print("idiot")
+#day 1.1
+print("hai")
+#day 1.2
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+#day 1.3
+print(len(input("your name")))
+#day 1.4
+a=input("a:")
+b=input("b:")
+c=b
+b=a
+a=c
+print(a+" "+b)
+#final
+print("band name gen")
+city=input("your place ")
+pet=input("name of the pet ")
+print(city+" "+pet)
